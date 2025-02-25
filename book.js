cd96c82dbf30247541d64390c341d3bf0107fe4e
darkhyper24 <s-mohammed.elmaadawy@zewailcity.edu.eg>
2025-02-26 00:24:20 +0200

    done with the task

---
+++ book.js
@@ -0,0 +1,24 @@
+let books = [
+    {
+        id: 1,
+        title: "harry potter"
+    },
+    {
+        id: 2,
+        title: "1919"
+    },
+    {
+        id: 3,
+        title: "The Great grandline"
+    },
+    {
+        id: 4,
+        title: "finding nemo"
+    },
+    {
+        id: 5,
+        title: "one piece"
+    }
+];
+
+module.exports = books;
